ea48aecadb991996a563f522f0166439ee814b3b
Debajoyti Paul <learn.ishan2001@gmail.com>
2024-11-11 07:05:24 +0000

    datatype -character

---
+++ 04_dataType/charcter_04.java
@@ -0,0 +1,11 @@
+public class charcter_04 {
+    public static void main(String[] args) {
+        char myFirstLatter = 'D';
+        System.err.println("My first name letter is:" + " " + myFirstLatter);
+
+        char value1 = 65, value2 = 66, value3 = 67;
+        System.out.println(value1);   // A
+        System.out.println(value2);  // B
+        System.out.println(value3); // C
+    }
+}

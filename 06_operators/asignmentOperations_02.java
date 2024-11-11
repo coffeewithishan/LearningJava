293a83b77948367701909441496b860bec23f92a
Debajoyti Paul <learn.ishan2001@gmail.com>
2024-11-11 14:52:32 +0000

    basic over view of Comparison Operators

---
+++ 06_operators/asignmentOperations_02.java
@@ -0,0 +1,16 @@
+public class asignmentOperations_02 {
+    public static void main (String[] args){
+        // Assignment operators
+        int x = 5;
+        int y = x + x;
+        int z = y ++;
+        int m = z--;
+        int n = m*3;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+        System.out.println("m = " + m);
+        System.out.println("n = " + n);
+        
+    }
+}
---
+++ 06_operators/comparisonOperators_03.java
@@ -0,0 +1,14 @@
+public class comparisonOperators_03 {
+    public static void main(String[] args) {
+        int a = 121;
+        int b = 111;
+    System.out.println(a > b); // true
+    System.out.println(a >= b); // true
+    System.out.println(a < b); // false
+    System.out.println(a <= b); // false
+    System.out.println(a == b); // false
+    System.out.println(a = b); // 111 (it gets assigned the value of b)
+    System.out.println(a != b); // true
+
+}
+    }

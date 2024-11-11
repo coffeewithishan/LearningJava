3cf210ce634ff724596e7300b0169ebb353690a3
Debajoyti Paul <learn.ishan2001@gmail.com>
2024-11-11 04:59:05 +0000

    basic printing numbers. in java only one class name could be public.

---
+++ 02_java_output/print_01.java
@@ -0,0 +1,17 @@
+public class print_01 {
+    public static void main(String[]args){
+        System.out.print("This is the print method 1");
+        System.out.println("This is the print method 2 with new line");
+        System.out.println("This is the print method 3 with new line");
+    }
+}
+
+class printingNumbers {
+    public static void main(String[] args) {
+        System.out.println(3 + 3);
+        System.out.println(3 - 3);
+        System.out.println(3 * 3);
+        System.out.println(3 / 3);
+    }
+    
+}

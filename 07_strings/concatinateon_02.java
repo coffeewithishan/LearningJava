d0ef7c2b242dc1df1c922a3a09fd5daeeab5dcce
Debajoyti Paul <learn.ishan2001@gmail.com>
2024-11-11 16:22:23 +0000

    basic overview of concatenation

---
+++ 07_strings/concatinateon_02.java
@@ -0,0 +1,13 @@
+public class concatinateon_02 {
+    public static void main(String[] args) {
+        String name = "Ishan";
+        String surName = "Paul";
+        String fullName = name + " " + surName;
+        System.out.println(fullName); // Ishan Paul
+        String a = "This";
+        String b = "is";
+        String c =  "Concatenation";
+        String result = a + " " + b + " " + c;
+        System.out.println(result);
+    }
+}

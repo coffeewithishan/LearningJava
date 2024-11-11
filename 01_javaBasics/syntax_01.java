a823addb7422f9d0f132ad947b5b3badce587687
Debajoyti Paul <learn.ishan2001@gmail.com>
2024-11-11 04:31:43 +0000

    This is the 1st java program. The class name and the file name should be same, the class name can not be start with numbers.

---
+++ 01_javaBasics/syntax_01.java
@@ -0,0 +1,8 @@
+public class syntax_01 {
+    public static void main(String[] args) {
+        System.out.println("Hello! This is Ishan's first java code");
+    }
+}
+// tips => class name must be = file name.
+// every program must contain the main() method.
+

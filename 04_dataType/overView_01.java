f03d4b6559377cdad9c069a497bd0591de9ce128
Debajoyti Paul <learn.ishan2001@gmail.com>
2024-11-11 06:14:29 +0000

    basic overview of java data type.

---
+++ 04_dataType/overView_01.java
@@ -0,0 +1,16 @@
+public class overView_01 {
+    public static void main(String[] args) {
+        // int, float, char, boolean, string;
+        int num = 100;
+        float num1 = 100.01f;
+        char myLetter = 'D';
+        boolean isTrue = true;
+        String greetings = "Hello every one";
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(isTrue);
+        System.out.println(greetings);
+        System.err.println(myLetter);
+        
+    }
+}

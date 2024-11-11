85d1632cead0d5ad992b68ffdd0489811e11ff8f
Debajoyti Paul <learn.ishan2001@gmail.com>
2024-11-11 06:57:09 +0000

    datatype - boolean

---
+++ 04_dataType/boolean_03.java
@@ -0,0 +1,8 @@
+public class boolean_03 {
+    public static void main (String[] args){
+        boolean isLoggedin = true;
+        boolean isAdmin = false;
+        boolean isUser = true;
+        System.out.println("Login status = " + " " + isLoggedin + "\nAdmin Status =" + " " + isAdmin + "\nUser status " + " " + isUser);
+    }
+}

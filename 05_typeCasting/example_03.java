3b0c38020b556732f44505687d3752594a6975dd
Debajoyti Paul <learn.ishan2001@gmail.com>
2024-11-11 14:08:07 +0000

    basic example

---
+++ 05_typeCasting/example_03.java
@@ -0,0 +1,8 @@
+class example_03 {
+    public static void main(String[] args) {
+        int subjectScore = 90;
+        int totalMarks = 150;
+        float percentage = (float) subjectScore / totalMarks * 100;
+        System.out.println(percentage);
+    }
+}

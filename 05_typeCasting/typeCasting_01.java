a98a8ef5ef64ef606fa5bd547c0febb5c48a12e0
Debajoyti Paul <learn.ishan2001@gmail.com>
2024-11-11 13:36:37 +0000

    example of Widening Casting

---
+++ 05_typeCasting/typeCasting_01.java
@@ -0,0 +1,19 @@
+/*Java Type Casting
+Type casting is when you assign a value of one primitive data type to another type.
+
+In Java, there are two types of casting:
+
+Widening Casting (automatically) - converting a smaller type to a larger type size
+byte -> short -> char -> int -> long -> float -> double
+
+Narrowing Casting (manually) - converting a larger type to a smaller size type
+double -> float -> long -> int -> char -> short -> byte*/
+
+public class typeCasting_01 {
+    public static void main(String [] args) {
+        int value = 90;
+        double valueDoubled = value;// converting the value automatically from int to double.
+        System.out.print("This is the integer value" + " " + value);
+        System.out.print("\nThis is the value after get doubled" + " " + valueDoubled + "\n");
+    }
+}

4811eb2d99a2f79fc4d982aa09f39b4dc13521e2
Debajoyti Paul <learn.ishan2001@gmail.com>
2024-11-11 05:53:39 +0000

    Basic overview of identifiers in java

---
+++ 03_variables/identifiers_03.java
@@ -0,0 +1,18 @@
+// All Java variables must be identified with unique names.
+
+// These unique names are called identifiers.
+
+// Identifiers can be short names (like x and y) or more descriptive names (age, sum, totalVolume).
+
+public class identifiers_03 {
+    public static void main(String[] args) {
+        Number x = 10;
+        System.out.println("This is okey but not so good" + " " +  x);
+
+        // try to use a variable name that is a keyword
+        String Name = "Ishan";
+        Number age = 23;
+        boolean isLoggedin = false;
+        System.out.println("My name is" + " " + Name + "\nMy age is" + " " + age + "\nI am logged in" + " " + isLoggedin);
+    }
+}

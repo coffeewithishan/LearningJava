94261f75a90390146240f5204629420aa72348bc
Debajoyti Paul <learn.ishan2001@gmail.com>
2024-11-12 04:40:14 +0000

    overview of if-else condition

---
+++ 09_conditionalStatement/else_02.java
@@ -0,0 +1,11 @@
+public class else_02 {
+    public static void main(String [] args){
+        int age = 16;
+        if (age >= 18) {
+            System.out.println("You can do the job");
+        }
+        else {
+            System.out.println("You can not do the job");
+        }
+    }
+}

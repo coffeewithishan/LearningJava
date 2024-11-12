7e1fd9e56d2851aa60f49dc9268282bfd41efa19
Debajoyti Paul <learn.ishan2001@gmail.com>
2024-11-12 04:36:20 +0000

    overview of if condition

---
+++ 09_conditionalStatement/if_01.java
@@ -0,0 +1,13 @@
+// example of if condition
+public class if_01{
+    public static void main(String[] args) {
+        int age = 19;
+        if (age >= 18) {
+            System.out.println("You are applicable" + " "+ age);
+        }
+        int age1 = 17;
+        if(age1 < 18){
+            System.out.println("You are not applicable" + " " + age1);
+        }
+    }
+}

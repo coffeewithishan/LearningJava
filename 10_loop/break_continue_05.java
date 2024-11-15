8973f1417270f3e3f2142656eaee35815a980e18
Debajoyti Paul <learn.ishan2001@gmail.com>
2024-11-15 07:49:01 +0000

    over view of break and continue

---
+++ 10_loop/break_continue_05.java
@@ -0,0 +1,27 @@
+// The break statement can also be used to jump out of a loop.
+
+// The continue statement breaks one iteration (in the loop), if a specified condition occurs, and continues with the next iteration in the loop.
+
+public class break_continue_05 {
+    public static void main (String [] args){
+        for(int i = 0; i <= 5; i++){
+            System.out.println(i);
+            if (i == 5) {
+                System.out.println("Loop breaks the value of = " + i);
+                break;
+            }
+            
+        }
+        // eg of continue
+        for(int k = 0; k <= 5; k ++){
+            //System.out.println(k);
+            if(k == 3){
+                
+                continue;
+            }
+            System.out.println(k);
+            
+        }
+    }
+}
+

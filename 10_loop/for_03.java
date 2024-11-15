14a7c8483c63af5d72729736498965dd8f763536
Debajoyti Paul <learn.ishan2001@gmail.com>
2024-11-15 06:42:42 +0000

    basic of for loop

---
+++ 10_loop/for_03.java
@@ -0,0 +1,15 @@
+/*for (statement 1; statement 2; statement 3) {
+  // code block to be executed
+} */
+
+public class for_03 {
+    public static void main (String [] args){
+        for (int i = 0; i <= 10; i++){
+            System.out.println("This is the for loop, the value of i:" + " " +  i);
+        }
+        int n = 10;
+        for (int j = 0; j <= n; j++){
+            System.out.println("j = " + j);
+        }
+    }
+}
